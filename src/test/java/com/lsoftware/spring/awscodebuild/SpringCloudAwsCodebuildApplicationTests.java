dd1641e3a6d210fcca9c688906cdb183a80a3ea8
Luis Espinosa 7 <lluisllanoss@gmail.com>
2022-05-21 16:16:04 -0500

    Test with codebuild

---
+++ src/test/java/com/lsoftware/spring/awscodebuild/SpringCloudAwsCodebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsoftware.spring.awscodebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAwsCodebuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
